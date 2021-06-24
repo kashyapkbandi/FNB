00f70c7f2320c866abd784f479b6d8803da41bd7
kashyapkbandi <kashyapkbandi@users.noreply.github.com>
2021-06-24 09:43:23 +0530

    Update controller.js

--- assets/scripts/controller.js
+++ assets/scripts/controller.js
@@ -40,8 +40,6 @@ function fetchAllFNBItemsFromSF() {
                          }
                     );
     
-<<<<<<< HEAD
-}
-=======
-}
->>>>>>> 6e8f947109da89a862da0c2b952c3ca3e28a74ed
+ }
+ }
+ 
